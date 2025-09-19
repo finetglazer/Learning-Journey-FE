50d67a8978dc0ba2fd1d9c276699033fe71c2393
OLogN <vinhhiepbn2003.work@gmail.com>
2025-09-19 18:48:28 +0700

    Config NextJS & TailwindCSS & Shadcn Components 9/19/2025, 6:48:17 PM

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
-  ),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
